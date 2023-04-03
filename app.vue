95d70c55fe7d6743ba4ddc887cbd475e62d83433
henrycunh <henrycunh@gmail.com>
2023-04-03 20:55:02 -0300

    fix: prevent zoom-in in mobile (#25)

--- app.vue
+++ app.vue
@@ -3,8 +3,8 @@ useHead({
     title: 'geppeto',
     meta: [
         { name: 'description', content: 'Your brain on GPT' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1' },
     ],
-    // Favicon SVG
     link: [
         { rel: 'icon', type: 'image/svg+xml', href: 'https://api.iconify.design/fluent-emoji:robot.svg?color=%230fbd3b' },
     ],
